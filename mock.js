8b1de794c5b027a0dcf188c079fecddeaa5a01bd
Nooneb <1971902975@qq.com>
2024-05-18 23:07:56 +0800

    api更换大量

---
+++ mock.js
@@ -0,0 +1,6 @@
+
+let mockAuth="Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2NDhiMGE2ZmEyOTQwMTQ3ODQzNzNmOSIsImlhdCI6MTcxNjAzOTg0NiwiZXhwIjoxNzE3MjQ5NDQ2fQ.QTh9yilYpgssRjsSPEEyUF0LvNJ7NAJss-4giP-EMXQ";
+
+if(typeof(exports) != "undefined"){
+    exports.mockAuth= mockAuth;
+}
